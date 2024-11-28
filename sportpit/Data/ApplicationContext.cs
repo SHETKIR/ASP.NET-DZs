3050b06f49168b78c4a302ac514cba0f66ddd87b
SHETKIR <98348096+SHETKIR@users.noreply.github.com>
2024-11-28 20:09:58 +0400

    Add files via upload

---
+++ sportpit/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportPit.Data.EntityTypeConfigurations;
+using SportPit.Extensions;
+using SportPit.Models;
+namespace SportPit.Data;
+
+public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
+{
+    public required DbSet<Product> Products { get; set; }
+    public required DbSet<Cart> Carts { get; set; }
+    public required DbSet<User> Users { get; set; }
+    public required DbSet<Category> Category { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyAllConfigurations();
+        base.OnModelCreating(modelBuilder);
+    }
+}
