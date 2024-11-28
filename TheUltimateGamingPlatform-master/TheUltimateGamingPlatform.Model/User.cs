14cd7dc6f7779214355de187d1334f175454044f
SHETKIR <98348096+SHETKIR@users.noreply.github.com>
2024-11-28 19:58:47 +0400

    Delete TheUltimateGamingPlatform-master directory

--- TheUltimateGamingPlatform-master/TheUltimateGamingPlatform.Model/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TheUltimateGamingPlatform.Model;
-
-public class User
-{
-    public int Id { get; set; }
-    public required string UserName { get; set; }
-    public required List<Game> Games { get; set; }
-}
--- TheUltimateGamingPlatform-master/TheUltimateGamingPlatform.Web/Repositories/CartGameRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using TheUltimateGamingPlatform.Model;
-
-namespace TheUltimateGamingPlatform.Web.Repositories;
-
-public class CartGameRepository
-{
-    public List<Game> Games { get; set; } = new();
-}
